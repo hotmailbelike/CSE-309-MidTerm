4861177fed1eb301ee4ac6c1f6b47ac32d626b31
hotmailbelike <fishilike.fish@gmail.com>
2019-10-21 22:32:18 +0600

    Updated Question 4

--- script.js
+++ script.js
@@ -7,29 +7,56 @@ $('#click').click(function() {
   // console.log(start);
   // console.log(end);
   // console.log(interval);
-  var c1, c2, c3, c4, c5;
-  c1 = 'blue';
-  c2 = 'green';
-  c3 = 'yellow';
-  c4 = 'orange';
-  c5 = 'red';
+  // var c1, c2, c3, c4, c5;
+  // c1 = 'blue';
+  // c2 = 'green';
+  // c3 = 'yellow';
+  // c4 = 'orange';
+  // c5 = 'red';
 
-  var i = 0;
-  function move() {
-    if (i == 0) {
-      i = 1;
-      var elem = document.getElementById('bar');
-      var width = 1;
-      var id = setInterval(frame, 10);
-      function frame() {
-        if (width >= 100) {
-          clearInterval(id);
-          i = 0;
-        } else {
-          width++;
-          elem.style.width = width + '%';
-        }
-      }
+  var size = 0;
+
+  $('#start-temperature').text(start + ' Degree Celcius');
+  $('#end-temperature').text(end + ' Degree Celcius');
+
+  var progress = setInterval(function() {
+    size += 20;
+    if (size === 20) {
+      $('#bar')
+        .addClass('bg-primary')
+        .css('width', size + '%');
+    } else if (size === 40) {
+      $('#bar')
+        .addClass('bg-success')
+        .css('width', size + '%');
+    } else if (size === 60) {
+      $('#bar')
+        .addClass('bg-warning')
+        .css('width', size + '%');
+    } else if (size === 80) {
+      $('#bar')
+        .addClass('progress-bar-orange')
+        .css('width', size + '%');
+    } else if (size === 100) {
+      $('#bar')
+        .removeClass('progress-bar-orange')
+        .addClass('bg-danger')
+        .css('width', size + '%');
     }
-  }
+
+    // $('#bar').css('width', size + '%');
+  }, interval);
 });
+
+// $(document).ready(function () {
+//   $('#start').click(function () {
+//     var size = 0;
+//     var interval = setInterval(function () {
+//       size = size + 10;
+//       $('.progress-bar-fill').css('width', size + '%');
+//       if (size >= 100) {
+//         clearInterval(interval);
+//       }
+//     }, 1000);
+//   });
+// });
